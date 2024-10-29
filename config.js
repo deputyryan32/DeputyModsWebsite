cb1aa594e94094794b48a10a2d4347bf40d0eb5e
DeputyRyan <rybug5210@gmail.com>
2024-10-29 19:42:04 -0400

    V1

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const config = {
+    theme: {
+        background: "#111", // Dark background color
+        textColor: "#fff",  // Main text color
+        buttonColor: "#222", // Button background color
+        linkColor: "#00aaff" // Button and link color
+    }
+};
